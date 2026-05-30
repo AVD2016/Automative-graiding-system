8b44def78ad24d8099b9f2b3725f936123a72665
AVD2016 <120407411+AVD2016@users.noreply.github.com>
2026-05-30 21:06:03 +0000

    method mapping fix getStudents()

--- Backend/src/main/java/uk/ac/rhul/cs2800/controller/AddStudentController.java
+++ Backend/src/main/java/uk/ac/rhul/cs2800/controller/AddStudentController.java
@@ -4,6 +4,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -49,7 +50,7 @@ public class AddStudentController {
     return ResponseEntity.ok(savedStudent);
   }
   
-  @PostMapping("/getStudents")
+  @GetMapping("/getStudents")
   public ResponseEntity<List<StudentDTO>> getAllStudents() {
 
     List<Student> students = (List<Student>) studentRepository.findAll();
