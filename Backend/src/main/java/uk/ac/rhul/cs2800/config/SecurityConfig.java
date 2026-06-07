e24d1cba8ea3b24d9f07c42ddc9eeaf8a0f91950
AVD2016 <120407411+AVD2016@users.noreply.github.com>
2026-06-07 20:13:15 +0000

    security config to address the CORS error

--- Backend/src/main/java/uk/ac/rhul/cs2800/config/SecurityConfig.java
+++ Backend/src/main/java/uk/ac/rhul/cs2800/config/SecurityConfig.java
@@ -41,7 +41,7 @@ public class SecurityConfig {
 
     config.setAllowedHeaders(Arrays.asList("*"));
 
-    config.setAllowCredentials(false);
+    config.setAllowCredentials(true);
 
     UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
     source.registerCorsConfiguration("/**", config);
