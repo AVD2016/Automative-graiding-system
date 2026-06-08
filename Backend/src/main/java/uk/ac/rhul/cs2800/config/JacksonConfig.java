b7ff7498f790c9469cd366647b4f41241b7a5f15
AVD2016 <120407411+AVD2016@users.noreply.github.com>
2026-06-08 20:54:33 +0000

    attemp to fix the config

--- Backend/src/main/java/uk/ac/rhul/cs2800/config/JacksonConfig.java
+++ Backend/src/main/java/uk/ac/rhul/cs2800/config/JacksonConfig.java
@@ -1,8 +1,8 @@
 package uk.ac.rhul.cs2800.config;
 
+import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 
@@ -10,10 +10,10 @@ import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 public class JacksonConfig {
 
   @Bean
-  public ObjectMapper objectMapper() {
-    ObjectMapper mapper = new ObjectMapper();
-    mapper.registerModule(new JavaTimeModule());
-    mapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
-    return mapper;
+  public Jackson2ObjectMapperBuilderCustomizer jacksonCustomizer() {
+    return builder -> {
+      builder.modulesToInstall(new JavaTimeModule());
+      builder.featuresToDisable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+    };
   }
 }
