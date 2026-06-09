44f2e1eddcd2fda8d369326f985455ec7f8dc129
AVD2016 <120407411+AVD2016@users.noreply.github.com>
2026-06-09 20:12:38 +0000

    @autowired annotation that coused an error

--- Backend/src/main/java/uk/ac/rhul/cs2800/controller/ModuleController.java
+++ Backend/src/main/java/uk/ac/rhul/cs2800/controller/ModuleController.java
@@ -51,6 +51,7 @@ public class ModuleController {
   @Autowired
   private RegistrationRepository registrationRepository;
 
+  @Autowired
   private AssignmentSubmissionRepository assignmentSubmissionRepository;
 
   @GetMapping("/getAvailableModules/{studentId}")
